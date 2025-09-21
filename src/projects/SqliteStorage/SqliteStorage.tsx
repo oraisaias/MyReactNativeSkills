d147f66e50ea5a459f5e875d53eb99b4d6056cd8
Isaías Chávez Martínez <isaiaschavez.co@outlook.com>
2025-09-21 12:29:55 -0600

    iniciando

---
+++ src/projects/SqliteStorage/SqliteStorage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SqliteStorage = () => {
+  return (
+    <View>
+      <Text>SqliteStorage</Text>
+    </View>
+  )
+}
+
+export default SqliteStorage
